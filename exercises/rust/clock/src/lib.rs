8a16f1a29523ac6aaa442683e8cb77ef01942e88
GarfieldZHU <Garfield.bupt@gmail.com>
2021-03-06 21:02:02 +0800

    feat: implement clock

--- exercises/rust/clock/src/lib.rs
+++ exercises/rust/clock/src/lib.rs
@@ -1,15 +1,30 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock {
+            hours: hours % 24,
+            minutes: minutes % 60,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        let new_minutes = self.minutes + minutes;
+        let add_hours = new_minutes / 60;
+
+        let new_hours = self.hours + add_hours;
+        Clock::new(new_hours, new_minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
     }
 }

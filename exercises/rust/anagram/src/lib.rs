0aa62141dc9fa4036b4e4322bbc9585a0f6f8905
GarfieldZHU <Garfield.bupt@gmail.com>
2021-03-10 22:15:55 +0800

    feat: implement anagram

--- exercises/rust/anagram/src/lib.rs
+++ exercises/rust/anagram/src/lib.rs
@@ -1,9 +1,22 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut result = HashSet::new();
+    let lower_case_word = word.to_lowercase();
+    let mut word_chars: Vec<char> = lower_case_word.chars().collect::<Vec<char>>();
+    word_chars.sort();
+
+    for anagram in possible_anagrams {
+        let lower_case_anagram = anagram.to_lowercase();
+        if lower_case_word != lower_case_anagram {
+            let mut anagram_chars: Vec<char> = lower_case_anagram.chars().collect::<Vec<char>>();
+            anagram_chars.sort();
+
+            if anagram_chars.len() == word_chars.len() && word_chars == anagram_chars {
+                result.insert(*anagram);
+            }
+        }
+    }
+
+    result
 }

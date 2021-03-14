41fa26009eaae664576aa00aa724baa7c99adfcd
GarfieldZHU <Garfield.bupt@gmail.com>
2021-03-14 23:21:50 +0800

    feat: optimize by matching and vec comparison

--- exercises/rust/sublist/src/lib.rs
+++ exercises/rust/sublist/src/lib.rs
@@ -6,42 +6,38 @@ pub enum Comparison {
     Unequal,
 }
 
-fn is_same_list<T: PartialEq>(first_list: &[T], second_list: &[T]) -> bool {
-    for i in 0..first_list.len() {
-        if first_list[i] != second_list[i] {
-            return false;
-        }
-    }
-    return true;
-}
-
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    if first_list.len() == second_list.len() {
-        if is_same_list(first_list, second_list) {
-            return Comparison::Equal;
-        } else {
+    match (first_list, second_list) {
+        (l, r) if l.len() == r.len() => {
+            if l == r {
+                return Comparison::Equal;
+            } else {
+                return Comparison::Unequal;
+            }
+        },
+        (l, r) if l.len() < r.len() => {
+            let check_len = l.len();
+            let max_pos = r.len() - l.len() + 1;
+            for  i in 0..max_pos {
+                let check_list= &r[i..(i+check_len)];
+                if l == check_list {
+                    return Comparison::Sublist;
+                }
+            }
             return Comparison::Unequal;
-        }
-    } else if first_list.len() < second_list.len() {
-        let check_len = first_list.len();
-        let max_pos = second_list.len() - first_list.len() + 1;
-        for  i in 0..max_pos+0 {
-            let check_list= &second_list[i..(i+check_len)];
-            if is_same_list(first_list, check_list) {
-                return Comparison::Sublist;
+    
+        },
+        (l, r) if l.len() > r.len() => {
+            let check_len = r.len();
+            let max_pos = l.len() - r.len() + 1;
+            for  i in 0..max_pos {
+                let check_list = &l[i..(i+check_len)];
+                if r == check_list {
+                    return Comparison::Superlist;
+                }
             }
-        }
-        return Comparison::Unequal;
-
-    } else {
-        let check_len = second_list.len();
-        let max_pos = first_list.len() - second_list.len();
-        for  i in 0..max_pos {
-            let check_list = &second_list[i..(i+check_len)];
-            if is_same_list(first_list, check_list) {
-                return Comparison::Sublist;
-            }
-        }
-        return Comparison::Unequal;
+            return Comparison::Unequal;
+        },
+        _ => return Comparison::Unequal,
     }
 }

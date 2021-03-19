21e2d81962daf496472f38b3e5dbbd20ef3f8e84
GarfieldZHU <Garfield.bupt@gmail.com>
2021-03-19 23:06:06 +0800

    feat: implment luhn

--- exercises/rust/luhn/src/lib.rs
+++ exercises/rust/luhn/src/lib.rs
@@ -1,4 +1,16 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    code.chars()
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|ch, index| {
+            match index {
+                even if index % 2 == 0 => {
+                    ch.to_digit(10)
+                },
+                odd if index % 2 != 0 => {
+                    ch.to_digit(10)
+                }
+            }
+        })
 }

2b8c382e6c82edd4036e9468abd4cd94b18d4694
GarfieldZHU <Garfield.bupt@gmail.com>
2021-03-20 01:01:29 +0800

    feat: implment luhn for most cases

--- exercises/rust/luhn/src/lib.rs
+++ exercises/rust/luhn/src/lib.rs
@@ -1,16 +1,34 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    code.chars()
-        .collect::<Vec<_>>()
+    let striped_code = code.chars().filter(|c| !c.is_whitespace()).collect::<Vec<_>>();
+    if striped_code.len() < 2 || striped_code.iter().any(|ch|
+        match ch.to_digit(10) {
+            Some(_digit) => false,
+            None => true,
+        }
+    ) { return false; }
+
+    println!("----{:?}", striped_code);
+
+
+    let sum = striped_code
         .iter()
-        .map(|ch, index| {
-            match index {
-                even if index % 2 == 0 => {
-                    ch.to_digit(10)
+        .enumerate()
+        .fold(0, |acc, (index, ch)| {
+            println!(">>>{} == {}", index, ch);
+
+            let digit = match index {
+                even if even % 2 != 0 => {
+                    ch.to_digit(10).unwrap()
                 },
-                odd if index % 2 != 0 => {
-                    ch.to_digit(10)
+                odd if odd % 2 == 0 => {
+                    let x = ch.to_digit(10).unwrap() * 2;
+                    if x > 9 { x - 9 } else { x }
                 }
-            }
-        })
+                _ => 0,
+            };
+            acc + digit
+        });
+
+    sum % 10 == 0
 }

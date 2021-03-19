46f9eb9396474148e5d96b547c1467b9f9beef05
GarfieldZHU <Garfield.bupt@gmail.com>
2021-03-20 01:09:49 +0800

    fix: consider digit index from right to left

--- exercises/rust/luhn/src/lib.rs
+++ exercises/rust/luhn/src/lib.rs
@@ -1,6 +1,9 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let striped_code = code.chars().filter(|c| !c.is_whitespace()).collect::<Vec<_>>();
+    let striped_code = code
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<Vec<_>>();
     if striped_code.len() < 2 || striped_code.iter().any(|ch|
         match ch.to_digit(10) {
             Some(_digit) => false,
@@ -8,20 +11,16 @@ pub fn is_valid(code: &str) -> bool {
         }
     ) { return false; }
 
-    println!("----{:?}", striped_code);
-
-
     let sum = striped_code
         .iter()
+        .rev()
         .enumerate()
         .fold(0, |acc, (index, ch)| {
-            println!(">>>{} == {}", index, ch);
-
             let digit = match index {
-                even if even % 2 != 0 => {
+                even if even % 2 == 0 => {
                     ch.to_digit(10).unwrap()
                 },
-                odd if odd % 2 == 0 => {
+                odd if odd % 2 != 0 => {
                     let x = ch.to_digit(10).unwrap() * 2;
                     if x > 9 { x - 9 } else { x }
                 }

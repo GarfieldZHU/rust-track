2d895570c3a0c39b3d7b8cbfc04656506024c4d2
GarfieldZHU <Garfield.bupt@gmail.com>
2021-03-22 00:41:15 +0800

    feat: implement basic multiple thread for counting

--- exercises/rust/parallel-letter-frequency/src/lib.rs
+++ exercises/rust/parallel-letter-frequency/src/lib.rs
@@ -1,12 +1,32 @@
-use std::collections::HashMap;
+use std::{collections::HashMap};
+use std::sync::{Arc, Mutex, RwLock};
 
-pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    unimplemented!(
-        "Count the frequency of letters in the given input '{:?}'. Ensure that you are using {} to process the input.",
-        input,
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{} workers", worker_count),
-        }
-    );
+pub fn frequency(input: &'static[&'static str], worker_count: usize) -> HashMap<char, usize> {
+    let mut handles = Vec::new();
+    let shared_map = Arc::new(RwLock::new(HashMap::new()));
+    let shared_iters = Arc::new(Mutex::new(input.iter()));
+
+    for _i in 0..worker_count {
+        let thread_map = Arc::clone(&shared_map);
+        let thread_iter = shared_iters.clone();
+
+        let handle = std::thread::spawn(move || {
+            while let Some(value) = thread_iter.lock().unwrap().next() {
+               value.chars().for_each(|ch| {
+                   let mut map = thread_map.read().unwrap();
+
+                    *map.entry(ch).or_insert(1) += 1;
+               })
+            }
+
+        });
+
+        handles.push(handle);
+    }
+
+    for h in handles {
+        h.join().unwrap();
+    }
+
+    shared_map.as_ref()
 }

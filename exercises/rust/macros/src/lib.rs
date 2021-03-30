4e93cae5b5a9517fb1d00fb5958a1f5cdef69771
GarfieldZHU <Garfield.bupt@gmail.com>
2021-03-30 21:48:04 +0800

    refactor: update

--- exercises/rust/macros/src/lib.rs
+++ exercises/rust/macros/src/lib.rs
@@ -4,13 +4,13 @@ macro_rules! hashmap {
         ::std::collections::HashMap::new()
     };
     ( $( $k:expr => $v:expr ),+ $(,)? ) => {
-       {
+        {
             let mut temp_map = ::std::collections::HashMap::new();
             $(
                 temp_map.insert($k, $v);
             )*
             temp_map
-       }
+        }
     };
 }
 

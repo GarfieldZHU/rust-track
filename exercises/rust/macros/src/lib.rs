9692aa51e5a4ee29dc6a2096df5a06f29637290b
GarfieldZHU <Garfield.bupt@gmail.com>
2021-03-30 21:38:18 +0800

    fix: support blank map

--- exercises/rust/macros/src/lib.rs
+++ exercises/rust/macros/src/lib.rs
@@ -1,12 +1,16 @@
 #[macro_export]
 macro_rules! hashmap {
+    () => {
+        ::std::collections::HashMap::new()
+    };
     ( $( $k:expr => $v:expr ),+ $(,)? ) => {
        {
-            let mut temp_map = HashMap::new();
+            let mut temp_map = ::std::collections::HashMap::new();
             $(
-                temp_map.insert(k, v);
+                temp_map.insert($k, $v);
             )*
             temp_map
        }
     };
 }
+
